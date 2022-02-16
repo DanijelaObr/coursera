e4ecbe1092d0acd8a95edeac2a1e0cbb62c98085
Danijela Obradovic <danijela.obradovic@htecgroup.com>
2022-02-16 15:49:15 +0100

    Module 4 solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -19,7 +19,8 @@
     // STEP 8: Rewrite the 'speak' function such that it is attached to the
     // byeSpeaker object instead of being a standalone function.
     // See Lecture 52, part 2
-    var byeSpeaker = function speak(name) {
+    var byeSpeaker = {};
+    byeSpeaker.speak = function(name) {
         console.log(speakWord + " " + name);
     }
     window.byeSpeaker = byeSpeaker;
--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -11,7 +11,8 @@
 (function(window) {
 
     var speakWord = "Hello";
-    var helloSpeaker = function speak(name) {
+    var helloSpeaker = {};
+    helloSpeaker.speak = function(name) {
         console.log(speakWord + " " + name);
     }
 
